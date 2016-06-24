009881442a437b03f8a845d7a6974ba3945307ab
Sommerregen <sommerregen@users.noreply.github.com>
2016-06-24 19:18:15 +0200

    Support latest Grav + Admin RC releases and comply to PSR 0/2

--- themer.php
+++ themer.php
@@ -33,136 +33,133 @@ use RocketTheme\Toolbox\Event\Event;
  */
 class ThemerPlugin extends Plugin
 {
-  /**
-   * Return a list of subscribed events of this plugin.
-   *
-   * @return array    The list of events of the plugin of the form
-   *                      'name' => ['method_name', priority].
-   */
-  public static function getSubscribedEvents()
-  {
-    return [
-      'onPluginsInitialized' => ['onPluginsInitialized', 0],
-      'onGetPageTemplates' => ['onGetPageTemplates', 0],
-    ];
-  }
-
-  /**
-  * Initialize configuration
-  */
-  public function onPluginsInitialized()
-  {
-    if ($this->config->get('plugins.themer.enabled')) {
-      $events = [
-        'onPageInitialized' => ['onPageInitialized', 1000]
-      ];
-
-      // Set admin specific events
-      if ($this->isAdmin()) {
-        $this->active = false;
-        $events = [
-          'onBlueprintCreated' => ['onBlueprintCreated', 0]
+    /**
+     * Return a list of subscribed events of this plugin.
+     *
+     * @return array    The list of events of the plugin of the form
+     *                      'name' => ['method_name', priority].
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
         ];
-      }
-
-      // Register events
-      $this->enable($events);
     }
-  }
 
-  /**
-   * Change theme of page
-   */
-  public function onPageInitialized()
-  {
-    /** @var Page $page */
-    $page = $this->grav['page'];
+    /**
+    * Initialize configuration
+    */
+    public function onPluginsInitialized()
+    {
+        $events = [
+            'onPageInitialized' => ['onPageInitialized', 1000]
+        ];
 
-    $config = $this->mergeConfig($page);
-    if ($config->get('enabled') && ($theme = $this->mergeThemeConfig($page))) {
-      if ($theme !== $this->config->get('system.pages.theme')) {
-        // Update system configurations
-        $this->config->set('system.pages.theme', $theme);
-
-        // Reload themes to reflect changes
-        $this->grav['themes']->init();
-
-        // Reset and re-initialize Twig environment
-        $twig = $this->grav['twig'];
-        $twig->twig = null;
-        $twig->twig_paths = [];
-        $twig->init();
-      }
-    }
-  }
-
-  /**
-   * Add page template types.
-   */
-  public function onGetPageTemplates(Event $event)
-  {
-    if ($this->config->get('plugins.themer.enabled')) {
-      /** @var Types $types */
-      $types = $event->types;
-
-      /** @var Locator $locator */
-      $locator = $this->grav['locator'];
-
-      // Add theme templates to list
-      $templates = $this->config->get('plugins.themer.templates', '');
-      if ($templates && !is_array($templates)) {
-        $templates = explode(', ', $templates);
-      }
-
-      $templates = $templates ?: array_keys($this->grav['themes']->all());
-      foreach ($templates as $template) {
-        if ($path = $locator->findResource("themes://{$template}/templates/")) {
-          $types->scanTemplates($path);
-        }
-      }
-    }
-  }
-
-  /**
-   * Extend page blueprints with ArchivePlus configuration options.
-   *
-   * @param Event $event
-   */
-  public function onBlueprintCreated(Event $event)
-  {
-    /** @var Blueprints $blueprint */
-    $blueprint = $event['blueprint'];
-    if ($blueprint->get('form.fields.tabs')) {
-      $blueprints = new Blueprints(__DIR__ . '/blueprints/');
-      $extends = $blueprints->get($this->name);
-      $blueprint->extend($extends, true);
-    }
-  }
-
-  /**
-   * Merge global and page theme settings
-   *
-   * @param Page  $page    The page to merge the page theme configurations
-   *                       with the theme settings.
-   * @param bool  $default The default value in case no theme setting was
-   *                       found.
-   *
-   * @return array
-   */
-  protected function mergeThemeConfig(Page $page, $default = null)
-  {
-    while ($page && !$page->root()) {
-      if (isset($page->header()->theme)) {
-        $theme = $page->header()->theme;
-        if ($theme === '@default') {
-          $theme = $default;
+        // Set admin specific events
+        if ($this->isAdmin()) {
+            $this->active = false;
+            $events = [
+                'onBlueprintCreated' => ['onBlueprintCreated', 0]
+            ];
         }
 
-        return $theme;
-      }
-      $page = $page->parent();
+        // Register events
+        $events['onGetPageTemplates'] = ['onGetPageTemplates', 0];
+        $this->enable($events);
     }
 
-    return $default;
-  }
+    /**
+     * Change theme of page
+     */
+    public function onPageInitialized()
+    {
+        /** @var Page $page */
+        $page = $this->grav['page'];
+
+        $config = $this->mergeConfig($page);
+        if ($config->get('enabled') && ($theme = $this->mergeThemeConfig($page))) {
+            if ($theme !== $this->config->get('system.pages.theme')) {
+                // Update system configurations
+                $this->config->set('system.pages.theme', $theme);
+
+                // Reload themes to reflect changes
+                $this->grav['themes']->init();
+
+                // Reset and re-initialize Twig environment
+                $twig = $this->grav['twig'];
+                $twig->twig = null;
+                $twig->twig_paths = [];
+                $twig->init();
+            }
+        }
+    }
+
+    /**
+     * Add page template types.
+     */
+    public function onGetPageTemplates(Event $event)
+    {
+        /** @var Types $types */
+        $types = $event->types;
+
+        /** @var Locator $locator */
+        $locator = $this->grav['locator'];
+
+        // Add theme templates to list
+        $templates = $this->config->get('plugins.themer.templates', '');
+        if ($templates && !is_array($templates)) {
+            $templates = explode(', ', $templates);
+        }
+
+        $templates = $templates ?: array_keys($this->grav['themes']->all());
+        foreach ($templates as $template) {
+            $template = strtolower($template);
+            if ($path = $locator->findResource("themes://{$template}/templates/")) {
+                $types->scanTemplates($path);
+            }
+        }
+    }
+
+    /**
+     * Extend page blueprints with ArchivePlus configuration options.
+     *
+     * @param Event $event
+     */
+    public function onBlueprintCreated(Event $event)
+    {
+        /** @var Blueprints $blueprint */
+        $blueprint = $event['blueprint'];
+        if ($blueprint->get('form/fields/tabs')) {
+            $blueprints = new Blueprints(__DIR__ . '/blueprints/');
+            $extends = $blueprints->get($this->name);
+            $blueprint->extend($extends, true);
+        }
+    }
+
+    /**
+     * Merge global and page theme settings
+     *
+     * @param Page  $page    The page to merge the page theme configurations
+     *                       with the theme settings.
+     * @param bool  $default The default value in case no theme setting was
+     *                       found.
+     *
+     * @return array
+     */
+    protected function mergeThemeConfig(Page $page, $default = null)
+    {
+        while ($page && !$page->root()) {
+            if (isset($page->header()->theme)) {
+                $theme = $page->header()->theme;
+                if ($theme === '@default') {
+                    $theme = $default;
+                }
+
+                return $theme;
+            }
+            $page = $page->parent();
+        }
+
+        return $default;
+    }
 }
